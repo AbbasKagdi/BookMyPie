fff0dbd6338a231825c12d4095a36b5a336c9192
AbbasKagdi <abkagdibas@gmail.com>
2023-01-14 13:38:22 +0300

    admin panel

---
+++ database/seeders/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class Admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create a default admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_admin' => 1,
+            'remember_token' => Str::random(10)
+        ]);
+    }
+}
