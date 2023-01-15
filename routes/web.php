aca366af0ec818e1db8525e0c6e548501d06559d
AbbasKagdi <abkagdibas@gmail.com>
2023-01-15 09:56:10 +0300

    admin dashboard settings

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Admin middleware
+// 'admin.' prefix in all admin routes
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function (){
+    Route::get('/', [AdminController::class, 'index'])->name('index');
 });
 
 require __DIR__.'/auth.php';
