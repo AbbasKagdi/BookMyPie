274fe16d76d4ff2220b41320dfb154b764bac17c
AbbasKagdi <abkagdibas@gmail.com>
2023-02-02 12:24:54 +0300

    sidebar + admin link in dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @if (Auth::user()->is_admin)
+                        <!-- Authentication -->
+                        <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                            {{ __('Admin') }}
+                        </x-nav-link>                     
+                    @endif
                 </div>
             </div>
 
