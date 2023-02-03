7b055b7cd69a4f26461c545904df839eaf1648b2
AbbasKagdi <abkagdibas@gmail.com>
2023-02-03 11:16:36 +0300

    resource controllers + views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,6 +37,12 @@ Route::middleware('auth')->group(function () {
 // 'admin.' prefix in all admin routes
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function (){
     Route::get('/', [AdminController::class, 'index'])->name('index');
+
+    // Restaurant resources
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/tables', TableController::class);
+    Route::resource('/reservation', ReservationController::class);
 });
 
 require __DIR__.'/auth.php';
